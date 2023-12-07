aed1ae27f160a178ac54353fc55f4c45364544cf
liaosiwei <2089548164@qq.com>
2023-12-07 19:47:08 +0800

    cors

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ import init from './init.js'
 const port = 3000
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  if (req.method === 'OPTIONS') res.send(200)
+  else {
+    next()
+  }
+})
+
 app.use(router)
 app.listen(port, () => {
   console.log(`app running on port ${port}!`)
