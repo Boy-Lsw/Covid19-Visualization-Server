5a6abd81794e317e83afca0e8b0ba2a5f7e11ebb
liaosiwei <2089548164@qq.com>
2023-12-10 15:57:43 +0800

    cities data type fixed

--- routes/cities.js
+++ routes/cities.js
@@ -2,8 +2,14 @@ import provinceSqlApi from '../database/sql/province.js'
 
 const cities = async (req, res) => {
   const _data = await provinceSqlApi.select.whichColumn(['cities'])
-  const data = _data.map(item => {
-    return JSON.parse(item.cities)
+  // const data = _data.map(item => {
+  //   return JSON.parse(item.cities)
+  // })
+  const data = []
+  _data.forEach(item => {
+    JSON.parse(item.cities).forEach(city => {
+      data.push(city)
+    })
   })
   res.send(data)
 }
